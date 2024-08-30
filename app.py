f04d74068f020f544629422c8348c9dd618e303f
João Matheus <joaomatheusars@gmail.com>
2024-08-30 05:00:28 -0300

    Implementando rotas CRUD

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from http import HTTPStatus
-from schemas import Message
+from schemas import Message, UserPublic, UserSchema, UserDB
 
 app = FastAPI()
 
+database = []
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)  
+
+    database.append(user_with_id)
+
+    return user_with_id
